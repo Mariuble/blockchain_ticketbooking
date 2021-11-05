06ddc2625b19be4c8346316349d6d87d68e00484
Dharshini Tharmarajan <dharshit@stud.ntnu.no>
2021-11-05 12:47:56 +0000

    Updated mint - TicketBookingSystem.sol

--- contracts/TicketBookingSystem.sol
+++ contracts/TicketBookingSystem.sol
@@ -20,6 +20,7 @@ contract TicketBookingSystem {
     Seat[] available_seats;
     address public owner;
     string information;
+    Ticket t = new Ticket();
 
     constructor(
         string memory _show_title,
@@ -35,18 +36,41 @@ contract TicketBookingSystem {
     function buy(address payable buyer, Seat memory seat) public {
         // Generate and transfer unique ticket
         uint256 balance = buyer.balance;
-        if (balance < seat.price) {
-            // throw;
-        }
+        require(balance < seat.price, "Balance is too low!");
+        t.mint(buyer);
     }
 }
 
 abstract contract Ticket is ERC721 {
     address public minter = msg.sender;
     Seat seat;
-
-    function mint(address to) public {
-        //
+    mapping (address => uint256) public balances;
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    tokenId=0;
+        // Mapping from holder address to their (enumerable) set of owned tokens
+    mapping (address => EnumerableSet.UintSet) private _holderTokens;
+    // Enumerable mapping from token ids to their owners
+    EnumerableMap.UintToAddressMap private _tokenOwners;
+    
+    
+    //Generate new token
+    /*
+    function mint(address to) internal virtual {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+        require(_checkOnERC721Received(address(0), to, tokenId, _data)
+        _holderTokens[to].add(tokenId);
+        _tokenOwners.set(tokenId, to);
+        emit Transfer(address(0), to, tokenId);
+    }
+    */
+    function mint(address recipient) internal virtual
+    returns (uint256)
+    {
+    unit256 newItemId= tokenId;
+    _mint(recipient, newItemId);
+    tokenId +=1;
+    return newItemId;
     }
 }
 
